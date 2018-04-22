72c38c6d178323d1248b562f5669261a3c009249
fuzzymee <rpmee@ncsu.edu>
2018-04-21 21:38:56 -0400

    Topic View Text fixed, RadioButtons for Voting Added
    -FIxed bug with view discussion button
    -Added radio buttons for choices and vote button
    -Text fields are no longer editable

--- app/src/main/java/com/google/firebase/udacity/friendlychat/PollDescriptionActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/PollDescriptionActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +24,7 @@ import java.util.Map;
 public class PollDescriptionActivity extends AppCompatActivity {
 
     private String poll_key;
+    private Button viewDiscussion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,7 @@ public class PollDescriptionActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         poll_key = intent.getStringExtra(getString(R.string.poll_id));
+        viewDiscussion = (Button)findViewById(R.id.postPollButton);
 
         // Capture the layout's TextView and set the string as its text
 
@@ -38,10 +43,11 @@ public class PollDescriptionActivity extends AppCompatActivity {
         DatabaseReference mMessagesDatabaseReference;
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("polls").child(poll_key);
-        final TextView titleView = (TextView) findViewById(R.id.titleEditText);
-        final TextView descriptionView = (TextView) findViewById(R.id.descriptionEditText);
-        final TextView option1View = (TextView) findViewById(R.id.option1EditText);
-        final TextView option2View = (TextView) findViewById(R.id.option2EditText);
+        final TextView titleView = (TextView) findViewById(R.id.titleText);
+        final TextView descriptionView = (TextView) findViewById(R.id.descriptionText);
+        final RadioGroup choices = (RadioGroup) findViewById(R.id.choices);
+        final RadioButton choice1 = (RadioButton) findViewById(R.id.choice1);
+        final RadioButton choice2 = (RadioButton) findViewById(R.id.choice2);
 
         mMessagesDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -50,13 +56,21 @@ public class PollDescriptionActivity extends AppCompatActivity {
                 System.out.print(poll);
                 titleView.setText(poll.getTitle());
                 descriptionView.setText(poll.getDescription());
-                option1View.setText(poll.getOption1());
-                option2View.setText(poll.getOption2());
+                choice1.setText(poll.getOption1());
+                choice2.setText(poll.getOption2());
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+
+
+        viewDiscussion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDiscussion(view);
             }
         });
 
